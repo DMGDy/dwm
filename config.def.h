5d51b45682ae6871ed31160a926fba1d92ec3fcc
DMGDy <dylangarza1909@gmail.com>
2024-01-22 19:26:18 -0500

    anybar patch

--- config.def.h
+++ config.def.h
@@ -10,13 +10,10 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-//static const char *altbarclass      = ""; /* Alternate bar class name */
-//static const char *alttrayname      = "";    /* Polybar tray instance name */
-//static const char *altbarcmd        = ""; /* Alternate bar launch command */
 static const char dmenufont[]       = 
 {"FiraCodeNerdFont-Retina:size=16"};
 
